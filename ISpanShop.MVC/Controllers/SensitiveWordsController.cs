21c29c6ecbd0dbf4af020fe292748cf8401bf58e
Berlin <a0965828360@gmail.com>
2026-03-10 04:35:39 +0800

    SensitiveWord Create

---
+++ ISpanShop.MVC/Controllers/SensitiveWordsController.cs
@@ -0,0 +1,59 @@
+﻿using ISpanShop.Models.DTOs;
+using ISpanShop.Models.EfModels;
+using ISpanShop.MVC.Models.ViewModels;
+using ISpanShop.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISpanShop.MVC.Controllers
+{
+	public class SensitiveWordsController : Controller
+	{
+		private readonly ISensitiveWordService _service;
+
+		public SensitiveWordsController(ISensitiveWordService service)
+		{
+			_service = service;
+		}
+
+		// --- 1. 列表 (Index) ---
+		public async Task<IActionResult> Index()
+		{
+			var dtos = await _service.GetAllAsync();
+			var vms = dtos.Select(d => new SensitiveWordVm
+			{
+				Id = d.Id,
+				Word = d.Word,
+				Category = d.Category,
+				IsActive = d.IsActive
+			}).ToList();
+			return View(vms);
+		}
+
+		// --- 2. 顯示新增頁面 (GET) ---
+		// 這裡通常加在 Index 之後
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// --- 3. 接收表單資料 (POST) ---
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(SensitiveWordVm vm)
+		{
+			if (ModelState.IsValid)
+			{
+				var dto = new SensitiveWordDto
+				{
+					Word = vm.Word,
+					Category = vm.Category,
+					IsActive = vm.IsActive
+				};
+				await _service.CreateAsync(dto);
+				return RedirectToAction(nameof(Index));
+			}
+			return View(vm);
+		}
+	} // <-- 確保所有 Action 都在這個大括號裡面
+}
---
+++ ISpanShop.MVC/Models/ViewModels/SensitiveWordVm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ISpanShop.MVC.Models.ViewModels
+{
+	public class SensitiveWordVm
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "請輸入敏感字內容")]
+		[StringLength(100, ErrorMessage = "長度不能超過 100 個字元")]
+		[Display(Name = "敏感字內容")]
+		public string Word { get; set; }
+
+		[Required(ErrorMessage = "請選擇分類")]
+		[Display(Name = "分類")]
+		public string Category { get; set; }
+
+		[Display(Name = "是否啟用")]
+		public bool IsActive { get; set; }
+	}
+}
