476670cb02ad177ef4d8f96a18c2f2fc32e190a4
Hua <b5586164@gmail.com>
2026-04-10 11:31:43 +0800

    新增前端資料夾架構，測試element-plus

--- ISpanShop-Frontend/src/main.ts
+++ ISpanShop-Frontend/src/main.ts
@@ -6,9 +6,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//啟用 Element Plus 並設定中文
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhTw from 'element-plus/es/locale/lang/zh-tw'  // 繁體中文
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+//註冊元件
+app.use(ElementPlus, {
+    locale: zhTw,
+})
 
 app.mount('#app')
